0f3268fd054916ac4c3eb55ca60f346668cb6d8c
thomasludovic01 <thomas.ludovic01@gmail.com>
2025-09-26 14:45:55 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const players = [
     { id: "nasra", name: "Nasra", avatar: "avatars/nasra.png", sqo: 0, partnerSqo: 1, weeklyWins: 0, bonusSteps: 3 },
     { id: "clement", name: "Clement", avatar: "avatars/clement.png", sqo: 1, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
     { id: "ahmad", name: "Ahmad", avatar: "avatars/ahmad.png", sqo: 1, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
-    { id: "valentin", name: "Valentin", avatar: "avatars/valentin.png", sqo: 2, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
+    { id: "valentin", name: "Valentin", avatar: "avatars/valentin.png", sqo: 3, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
     { id: "antho", name: "Antho", avatar: "avatars/antho.png", sqo: 0, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
 ];
 
