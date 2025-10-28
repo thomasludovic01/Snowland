6849a52af0bd023a737593ed7f295b7577f18ec3
thomasludovic01 <thomas.ludovic01@gmail.com>
2025-10-28 22:51:29 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 // --- ‼️ YOU ONLY NEED TO EDIT THIS SECTION ‼️ ---
 const players = [
-    { id: "lylia", name: "Lylia", avatar: "avatars/lylia.png", sqo: 5, partnerSqo: 3, weeklyWins: 1, bonusSteps: 5 },
+    { id: "lylia", name: "Lylia", avatar: "avatars/lylia.png", sqo: 6, partnerSqo: 3, weeklyWins: 4, bonusSteps: 5 },
     { id: "mara", name: "Mara", avatar: "avatars/mara.png", sqo: 4, partnerSqo: 3, weeklyWins: 0, bonusSteps: 3 },
-    { id: "pouya", name: "Pouya", avatar: "avatars/pouya.png", sqo: 4, partnerSqo: 3, weeklyWins: 2, bonusSteps: 3 },
-    { id: "nasra", name: "Nasra", avatar: "avatars/nasra.png", sqo: 3, partnerSqo: 1, weeklyWins: 0, bonusSteps: 3 },
-    { id: "clement", name: "Clement", avatar: "avatars/clement.png", sqo: 4, partnerSqo: 0, weeklyWins: 0, bonusSteps: 3 },
-    { id: "ahmad", name: "Ahmad", avatar: "avatars/ahmad.png", sqo: 2, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
-    { id: "valentin", name: "Valentin", avatar: "avatars/valentin.png", sqo: 3, partnerSqo: 2, weeklyWins: 0, bonusSteps: 0 },
-    { id: "antho", name: "Antho", avatar: "avatars/antho.png", sqo: 0, partnerSqo: 0, weeklyWins: 0, bonusSteps: 0 },
+    { id: "pouya", name: "Pouya", avatar: "avatars/pouya.png", sqo: 5, partnerSqo: 4, weeklyWins: 2, bonusSteps: 3 },
+    { id: "nasra", name: "Nasra", avatar: "avatars/nasra.png", sqo: 2, partnerSqo: 4, weeklyWins: 0, bonusSteps: 6 },
+    { id: "clement", name: "Clement", avatar: "avatars/clement.png", sqo: 6, partnerSqo: 0, weeklyWins: 0, bonusSteps: 6 },
+    { id: "ahmad", name: "Ahmad", avatar: "avatars/ahmad.png", sqo: 2, partnerSqo: 1, weeklyWins: 0, bonusSteps: 0 },
+    { id: "valentin", name: "Valentin", avatar: "avatars/valentin.png", sqo: 3, partnerSqo: 6, weeklyWins: 0, bonusSteps: 0 },
+    { id: "antho", name: "Antho", avatar: "avatars/antho.png", sqo: 3, partnerSqo: 1, weeklyWins: 0, bonusSteps: 0 },
 ];
 
 const specialSpaces = {
